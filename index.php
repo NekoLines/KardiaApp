c7527d60b6095ac2b91dbac8e105de21dcfa05d6
Koi <swpqianmst@hotmail.com>
2019-02-02 21:51:11 +0800

    初步完成手机视图，没有适配PC

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="zh">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport">
+        <title>卡库拉拉兹巴魔法棒 - 客户端下载 - 卡露蒂亚城乡结合部</title>
+        <link type="text/css" rel="stylesheet" href="css/style.css">
+    </head>
+    <body class="bg">
+        <div class="top-area">
+            <a class="logo" href="http://kardia.ac.cn"></a>
+            <h1 class="headtext">欢迎食用<span>炼金模拟器</span></h1>
+        </div>
+        <div class="down-button-area">
+            <a class="btn-download" id="btn-android-download" href="app/simu_1.0.apk">
+                <p class="title">Android</p>
+                <p class="version">1.0.0</p>
+                <p class="date">@ 2018/12/25</p>
+                <p></p>
+                <img src="bg/icon-android.png" class="elem-icon"/>
+            </a>
+            <a class="btn-download" id="btn-ios-download" href="http://kardia.ac.cn/simu/">
+                <p class="title">IOS</p>
+                <p class="version">尚未制作，敬请期待</p>
+                <p class="date">@ 8102/12/25</p>
+                <p></p>
+                <img src="bg/icon-ios.png" class="elem-icon"/>
+            </a>
+        </div>
+        <div class="footer">
+            <p class="copyright">本网站使用素材版权归制作者所有</p>
+            <p class="copyright">卡库拉拉兹巴魔法屋 2017 - 2019</p>
+        </div>
+    </body>
+</html>
